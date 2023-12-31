aca4b8ad700a10fda9a38acc8647f41c2eea0049
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2023-12-31 07:21:07 -0500

    Update FacebookGamesFix.user.js

--- mom/FacebookGamesFix.user.js
+++ mom/FacebookGamesFix.user.js
@@ -1,15 +1,42 @@
 // ==UserScript==
 // @name         FB Games
-// @version      0.1
+// @version      0.2
 // @description  Fix Facebook Games Issues
 // @author       You
 // @match        https://apps.facebook.com/*/*
+// @match        https://aws.rjs.in/fblexulous/*
 // @require      https://code.jquery.com/jquery-3.4.1.min.js
 // @run-at       document-end
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
 
 var alreadyRunning = false;
+var data = {};
+var dataPages = {
+    lexulous: { delay: 1000 }
+};
+
+function startup()
+{
+    fix( 10 );
+
+    let func = null;
+    for( let page of Object.keys( dataPages ) )
+    {
+        if( window.location.href.includes( page ) )
+        {
+            data = GM_getValue( 'data', {} );
+            func = page;
+            console.log( data );
+        }
+    }
+
+    if( func )
+    {
+        eval( `${func}()` );
+    }
+}
 
 function fix( nTimes )
 {
@@ -38,4 +65,52 @@ function fix( nTimes )
     }
 }
 
-fix( 10 );
+function lexulous()
+{
+    /*
+        TODO
+        - split list at selected game (not starting at first game)
+        - play => next button update
+    */
+
+    if( window.location.href.includes( 'apps.facebook' ) ) // runs in iframe
+        return;
+
+    if( $( '#wrapper' ).length )
+    {
+        if( $( 'a:contains("Next Game")' ).length ) // game
+        {
+            let lnk = $( 'a:contains("Next Game")' );
+            let next = data.lexulous.games.shift();
+            lnk.attr( 'href', next );
+
+            data.lexulous.games.push( next );
+            GM_setValue( 'data', data );
+
+            return;
+        }
+
+        // games list
+        let gameData = [];
+        let games = $( 'div.user_detailsrow' );
+        $.each( games, function( ndx, val ) {
+            let elem = $( val ).find( 'div[onclick*="viewboard"]' );
+            if( elem.length && elem.text().includes( 'Your Turn' ) )
+                gameData.push( $( elem ).attr( 'onclick' ).split( 'changePage(\'' )[1].split( '\');' )[0].trim() );
+        } );
+
+        if( gameData.length )
+        {
+            gameData.push( gameData.shift() ); // temp move first to last
+            data.lexulous = data.lexulous || {};
+            data.lexulous.games = gameData;
+            GM_setValue( 'data', data );
+
+            return;
+        }
+    }
+
+    setTimeout( lexulous, dataPages.lexulous.delay );
+}
+
+startup();
